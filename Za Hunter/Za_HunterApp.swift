085c0e266481ab20eb2fd7aae06b2a5829dc706e
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-03-08 10:28:08 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Student on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
